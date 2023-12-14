962b7be19f5c18bbd888de0fa60e221293c12aa1
kalkidanyishakz <kalkidanyishak002@gmail.com>
2023-12-14 16:44:54 +0300

    car saved

--- week5/car.py
+++ week5/car.py
@@ -8,29 +8,7 @@ class Car:
         print(f"Make: {self.make}\nModel: {self.model}\nYear: {self.year}")
 
 
-class ElectricCar(Car):
-    def __init__(self, make, model, year, battery_capacity):
-        super().__init__(make, model, year)
-        self.battery_capacity=battery_capacity
-        self.charge=0
-    
-    def charge(self, charge_amount):
-        if self.charge+charge_amount>=self.battery_capacity:
-            self.charge=self.battery_capacity
-        else:
-            self.charge+=charge_amount
-        
-    def display_info(self):
-        super().display_info()
-        print(f'battery capacity: {self.battery_capacity}')
-        print(f'charge: {self.charge}')
-
-            
-
 
 my_car = Car("Toyota", "Corolla", 2022)
 my_car.display_info()
-
-my_car2 = ElectricCar("Toyota", "Corolla", 2022, 200)
-my_car2.display_info()
 
